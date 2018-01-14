cfbbf2bc26948bbceb15d3deab0ec3d31b79ed86
Matteo Salmistraro <msalmi86@gmail.com>
2018-01-14 17:32:19 +0100

    Added timing on main.cpp. Updated README.md

--- Problem Set 2/README.md
+++ Problem Set 2/README.md
@@ -3,7 +3,8 @@ Instructions on how to invoke the program:
 - Inside the directory inkove: 
 	- cmake ..
 	- make
-- Now you can execute the program typing, inside the directory Make
+- Now you can execute the program typing, inside the directory Make,
+
  ./HW2 ../cinque_terre_small.jpg
 
 All the student-provided logic is in student_func.cu. The only other change was done to main.cpp to print also the timing of the reference code.
--- Problem Set 2/student_func.cu
+++ Problem Set 2/student_func.cu
@@ -140,17 +140,17 @@ void gaussian_blur(const unsigned char* const inputChannel,
 
    float result = .0f;
 
-   for (int filter_r = -filterWidth/2; filter_r <= filterWidth/2; ++filter_r) {
-        for (int filter_c = -filterWidth/2; filter_c <= filterWidth/2; ++filter_c) {
+   for (int filter_r = -filterWidth / 2; filter_r <= filterWidth / 2; ++filter_r) {
+        for (int filter_c = -filterWidth / 2; filter_c <= filterWidth / 2; ++filter_c) {
           //Find the global image position for this filter position
           //clamp to boundary of the image
-	  int image_r = min(max(r + filter_r, 0), static_cast<int>(numRows - 1));
-          int image_c = min(max(c + filter_c, 0), static_cast<int>(numCols - 1));
+	      const int image_r = min(max(r + filter_r, 0), static_cast<int>(numRows - 1));
+          const int image_c = min(max(c + filter_c, 0), static_cast<int>(numCols - 1));
 
-          float image_value = static_cast<float>(inputChannel[image_r * numCols + image_c]);
-          float filter_value = filter[(filter_r + filterWidth/2) * filterWidth + filter_c + filterWidth/2];
+          const float imageValue = static_cast<float>(inputChannel[image_r * numCols + image_c]);
+          const float filterValue = filter[(filter_r + filterWidth / 2) * filterWidth + filter_c + filterWidth/2];
 
-          result += image_value * filter_value;
+          result += imageValue * filterValue;
         }
    }
    outputChannel[r * numCols + c] = static_cast<unsigned char>(result);
